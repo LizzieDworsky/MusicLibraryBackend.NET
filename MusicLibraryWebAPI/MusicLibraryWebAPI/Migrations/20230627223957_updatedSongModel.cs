3893f5886f6da979b5ca3e0fd0daf7f63a69398a
LizzieDworsky <amydworsky15@gmail.com>
2023-06-27 16:41:22 -0600

    added properties to the Song model and migrated updates

---
+++ MusicLibraryWebAPI/MusicLibraryWebAPI/Migrations/20230627223957_updatedSongModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicLibraryWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedSongModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Album",
+                table: "Songs",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Artist",
+                table: "Songs",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Songs",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Likes",
+                table: "Songs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Songs",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Songs",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Album",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Artist",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Songs");
+        }
+    }
+}
--- MusicLibraryWebAPI/MusicLibraryWebAPI/Models/Song.cs
+++ MusicLibraryWebAPI/MusicLibraryWebAPI/Models/Song.cs
@@ -6,5 +6,11 @@ namespace MusicLibraryWebAPI.Models
     {
         [Key]
         public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public int Likes { get; set; }
     }
 }
